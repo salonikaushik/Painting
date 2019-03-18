a82f81ebf3cfdf3e5167a15415586194d664c281
salonikaushik <salonisingh455275@gmail.com>
2019-03-18 13:25:49 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Painting_store</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <header>
+        <div class="container_header">
+            <div class="img_big">
+                <img src="images/logo-lg.png"/>
+            </div>
+            <nav class="navbar">
+                <ul>
+                    <li><a href="#">Tools</a></li>
+                    <li><a href="#">Lumber</a></li>
+                    <li><a href="#">Paint</a></li>
+                    <li><a href="#">Garden</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>    
+        </div>
+    </header>
+    <section>
+        <div class="container_section1">
+            <div class="scary">
+                <h1>Painting is scary</h1>  
+            </div>
+            <div class="para">
+                <p>But it doesn't have to be! Let our handy Virtual Paint Department help guide you through the process of choosing a color. Know the hue? You know what to do. Click the order button and we'll get the paint to you.</p>
+            </div>
+        </div>
+    </section>
+        <div class="container_section2">
+            <div class="color">
+                <h1>Color Guide</h1>
+                <p>Here at HouseStore, we take color seriously. In each of the following sections, well explore our base colors in swatches that incrementally change three values:<b>Hue, Saturation</b> and <strong>Lightness.</strong>You'll be able to pick from a wide variety of colors that all work well with each other because they stem from the same value.</p>
+            </div>
+        </div>
+    <footer>
+    
+    <footer>
+    
+</body>
+</html>
